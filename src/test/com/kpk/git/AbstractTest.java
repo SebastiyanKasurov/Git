6bd966733a0e09bafd83797d789f714fe4d4e310
Sebastiyan Kasurov <sebastiqn98@gmail.com>
2019-06-29 23:30:28 +0200

    use test profile configuration

--- src/test/com/kpk/git/AbstractTest.java
+++ src/test/com/kpk/git/AbstractTest.java
@@ -1,8 +1,14 @@
 package com.kpk.git;
 
 import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@ActiveProfiles("test")
 public class AbstractTest {
 	protected Assert A;
-
 }
